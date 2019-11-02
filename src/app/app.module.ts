597077fa7057b75c3950d130ba63efa3208cf7d0
sanggu <tkdrn8578@naver.com>
2019-11-02 16:39:51 +0900

    [MODIFY] 생성된 components 를 app.modules 에 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
