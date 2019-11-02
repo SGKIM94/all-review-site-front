08057f5eeb44ae63b37982d376ada8d6bc4b4b4b
sanggu <tkdrn8578@naver.com>
2019-11-02 16:51:23 +0900

    [MODIFY] components 기본 생성 style type 을 scss 로 변경에 따른 components 에 작성된 style 확장자 scss로 변경

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
 
