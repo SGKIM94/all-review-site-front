8347391d31d358cb2097df992aac7e4a7e0a65ff
sanggu <tkdrn8578@naver.com>
2019-11-04 23:11:33 +0900

    [MODIFY] login 의 Controller 부분 추가

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,134 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import * as $ from 'jquery';
+
+
+import { FuseConfigService } from '@fuse/services/config.service';
+import { fuseAnimations } from '@fuse/animations/index';
 
 @Component({
-  selector: 'app-login',
+  selector   : 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls  : ['./login.component.scss'],
+  animations : fuseAnimations
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy
+{
+  loginForm: FormGroup;
+  loginFormErrors: any;
 
-  constructor() { }
+  // Private
+  private _unsubscribeAll: Subject<any>;
 
-  ngOnInit() {
+  /**
+   * Constructor
+   *
+   * @param {FuseConfigService} _fuseConfigService
+   * @param {FormBuilder} _formBuilder
+   */
+  constructor(
+      private _fuseConfigService: FuseConfigService,
+      private _formBuilder: FormBuilder
+  )
+  {
+    this.openMenu();
+    // Configure the layout
+    this._fuseConfigService.config = {
+      layout: {
+        navbar : {
+          hidden: true
+        },
+        toolbar: {
+          hidden: true
+        },
+        footer : {
+          hidden: true
+        }
+      }
+    };
+
+    // Set the defaults
+    this.loginFormErrors = {
+      email   : {},
+      password: {}
+    };
+
+    // Set the private defaults
+    this._unsubscribeAll = new Subject();
   }
 
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * On init
+   */
+  ngOnInit(): void
+  {
+    this.loginForm = this._formBuilder.group({
+      email   : ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.loginForm.valueChanges
+        .pipe(takeUntil(this._unsubscribeAll))
+        .subscribe(() => {
+          this.onLoginFormValuesChanged();
+        });
+  }
+
+  /**
+   * On destroy
+   */
+  ngOnDestroy(): void
+  {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Public methods
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * On form values changed
+   */
+  onLoginFormValuesChanged(): void
+  {
+    for ( const field in this.loginFormErrors )
+    {
+      if ( !this.loginFormErrors.hasOwnProperty(field) )
+      {
+        continue;
+      }
+
+      // Clear previous errors
+      this.loginFormErrors[field] = {};
+
+      // Get the control
+      const control = this.loginForm.get(field);
+
+      if ( control && control.dirty && !control.valid )
+      {
+        this.loginFormErrors[field] = control.errors;
+      }
+    }
+  }
+
+  onSubmit(){
+  }
+
+  openMenu(){
+    $('body').addClass('noScroll');
+    if ($('.collapse').hasClass('collapse-active')) {
+      $('.collapse').removeClass('collapse-active');
+    }
+    else {
+      $('.collapse').addClass('collapse-active');
+    }
+  }
 }
