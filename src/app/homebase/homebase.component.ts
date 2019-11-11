76e9ee1e8e54179391a35fb9c247bdbee71e45d9
sanggu <tkdrn8578@naver.com>
2019-11-11 23:05:39 +0900

    [ADD] homebase components 의 ts 추가

---
+++ src/app/homebase/homebase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homebase',
+  templateUrl: './homebase.component.html',
+  styleUrls: ['./homebase.component.scss']
+})
+export class HomebaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
