f47ad548b32435487e8bb6bb3cfae4721c59036e
kimsanggu <tkdrn8578@paymint.co.kr>
2019-11-13 22:37:07 +0900

    [MODIFY] register ts 정리 작업 및 메서드 refactoring 작업 진행

--- src/app/register-new/register-new.component.ts
+++ src/app/register-new/register-new.component.ts
@@ -1,17 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {fuseAnimations} from '../../@fuse/animations';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Subject} from 'rxjs';
+import {FuseConfigService} from '../../@fuse/services/config.service';
 
 @Component({
   selector: 'app-register-new',
   templateUrl: './register-new.component.html',
-  styleUrls: ['./register-new.component.scss']
+  styleUrls: ['./register-new.component.scss'],
   animations : fuseAnimations
 })
-export class RegisterNewComponent implements OnInit {
+export class RegisterNewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  registerForm: FormGroup;
+  registerFormErrors: any;
 
-  ngOnInit() {
+  private _unsubscribeAll: Subject<any>;
+
+  constructor(
+      private _fuseConfigService: FuseConfigService,
+      private _formBuilder: FormBuilder,
+
+  ) {
+      this.openMenu();
+
   }
 
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  openMenu = () => {
+    this.addCollapseActiveClassWithout();
+
+  }
+
+  private addCollapseActiveClassWithout = () => {
+    const collapseElement = this.document.getElementById('collapse');
+    if (collapseElement.classList.contains('collapse-active')) {
+      collapseElement.classList.remove('collapse-active');
+    }
+
+    collapseElement.classList.add('collapse-active');
+  }
 }
