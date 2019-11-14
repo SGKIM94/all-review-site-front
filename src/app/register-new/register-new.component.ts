95e6fc3f364eb8fa9829bbe93fcae54d90c41e1f
sanggu <tkdrn8578@naver.com>
2019-11-14 22:23:01 +0900

    [MODIFY] register 화면에서 입력값에 대한 오류 처리 추가 및 Validation 추가

--- src/app/register-new/register-new.component.ts
+++ src/app/register-new/register-new.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {fuseAnimations} from '../../@fuse/animations';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Subject} from 'rxjs';
 import {FuseConfigService} from '../../@fuse/services/config.service';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-register-new',
@@ -19,30 +20,120 @@ export class RegisterNewComponent implements OnInit, OnDestroy {
 
   constructor(
       private _fuseConfigService: FuseConfigService,
-      private _formBuilder: FormBuilder,
-
-  ) {
+      private _formBuilder: FormBuilder) {
       this.openMenu();
+      this.setFuseConfig();
+      this.initializeRegisterErrors();
 
+  }
+
+  private initializeRegisterErrors = () => {
+    this.registerFormErrors = {
+      name: {},
+      phone: {},
+      mobile: {},
+      password: {},
+      passwordConfirm: {},
+    };
+  }
+
+  private setFuseConfig = () => {
+    this._fuseConfigService.config = {
+      layout: {
+        navbar: {
+          hidden: true
+        },
+        toolbar: {
+          hidden: true
+        },
+        footer: {
+          hidden: true
+        }
+      }
+    };
   }
 
   ngOnInit(): void {
+    this.registerForm = this._formBuilder.group({
+      name : ['', Validators.required],
+      email : ['', [Validators.required, Validators.email]],
+      mobile : ['', [Validators.required, Validators.pattern(/^\+?\d{10}$/)]],
+      password : ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirm : ['', [Validators.required, confirmPassword]]
+    });
+
+    this.registerForm.valueChanges
+        .pipe(takeUntil(this._unsubscribeAll))
+        .subscribe(() => {
+          this.onRegisterFormValuesChanged();
+        });
+  }
+
+  onRegisterFormValuesChanged = () => {
+    this.registerFormErrors.map((field) => {
+      const control = this.registerFormErrors.get(field);
+
+      this.checkFieldError(control, field);
+    });
+  }
+
+  private checkFieldError = (control, field) => {
+    if (control && control.dirty && !control.valid) {
+      this.registerFormErrors[field] = control.errors;
+    }
   }
 
   ngOnDestroy(): void {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 
   openMenu = () => {
     this.addCollapseActiveClassWithout();
-
   }
 
   private addCollapseActiveClassWithout = () => {
-    const collapseElement = this.document.getElementById('collapse');
+    const collapseElement = document.getElementById('collapse');
     if (collapseElement.classList.contains('collapse-active')) {
       collapseElement.classList.remove('collapse-active');
     }
 
     collapseElement.classList.add('collapse-active');
   }
+}
+
+function confirmPassword(control: AbstractControl): any {
+  if (isControlEmpty(control)) {
+    return;
+  }
+
+  const parentControl = control.parent;
+  const password = parentControl.get('password');
+  const passwordConfirm = parentControl.get('passwordConfirm');
+
+  if (isPasswordAndPasswordConfirmEmpty(password, passwordConfirm)) {
+    return;
+  }
+
+  if (isMatchPasswordWithPasswordConfirm(password, passwordConfirm)) {
+    return {
+      passwordNotMatch: true
+    };
+  }
+
+  return {
+    passwordNotMatch: false
+  };
+}
+
+function isControlEmpty(control: AbstractControl): boolean {
+  return !control.parent || !control;
+}
+
+function isPasswordAndPasswordConfirmEmpty(password, passwordConfirm): boolean {
+  return !passwordConfirm || !password || passwordConfirm.value === '';
+}
+
+function isMatchPasswordWithPasswordConfirm(password, passwordConfirm): boolean {
+  return password.value !== passwordConfirm.value;
 }
