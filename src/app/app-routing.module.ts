a0c4a1959e1e0fb1b5e5b75b8f25806fd28061e6
sanggu <tkdrn8578@naver.com>
2019-11-20 23:01:11 +0900

    [MODIFY] question, answer components 를 routing 에 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {QuestionComponent} from './question/question.component';
+import {AnswerComponent} from './answer/answer.component';
 
 
 const routes: Routes = [
     {path : '', component: HomeComponent},
     {path : 'login', component : LoginComponent},
-    {path : 'register', component : RegisterComponent}
+    {path : 'register', component : RegisterComponent},
+    {path : 'question', component : QuestionComponent},
+    {path : 'answer', component : AnswerComponent},
+
 ];
 
 @NgModule({
