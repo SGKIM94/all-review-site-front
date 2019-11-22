232dbc6a07da782a44e028488ba26488cbd1903e
sanggu <tkdrn8578@naver.com>
2019-11-22 18:34:24 +0900

    [MODIFY] loginComponent 에 TestBed 로 component 를 받아서 생성된 TestBed 를 검증하는 방식으로 수정
     * 참고사이트 : https://codecraft.tv/courses/angular/unit-testing/angular-test-bed/

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let loginComponent: LoginComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,21 +17,26 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    loginComponent = TestBed.get(LoginComponent);
+
     fixture.detectChanges();
   });
 
   it('email 값이 올바르지 않을 때 error 가 loginFormErrors 에 저장되는지', () => {
-    fixture = TestBed.createComponent(LoginComponent);
-
+    // given
     const control = {
       dirty: true,
-      valid: false
+      valid: false,
+      errors: 'error',
     };
 
     const field = 'field';
 
-    component.checkFieldError(control, field);
-    expect(component).toHaveBeenCalled();
-    expect(component).toBeTruthy();
+    // when
+    loginComponent.checkFieldError(control, field);
+
+    // then
+    expect(loginComponent.checkFieldError).toHaveBeenCalled();
+    expect(loginComponent.loginFormErrors[field]).toBeTruthy('error');
   });
 });
