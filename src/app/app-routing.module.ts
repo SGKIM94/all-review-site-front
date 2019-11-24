8054cfb1615ee23b66558b539f4a03350ea37f61
sanggu <tkdrn8578@naver.com>
2019-11-24 22:23:03 +0900

    [MODIFY] router 가 안되는 문제로 기존에 사용하던 router 제거하고 CLI 로 다시 새로 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {QuestionComponent} from './question/question.component';
-import {AnswerComponent} from './answer/answer.component';
+import {HomeComponent} from './home/home.component';
 
 
 const routes: Routes = [
-    {path : '', component: HomeComponent},
-    {path : 'login', component : LoginComponent},
-    {path : 'register', component : RegisterComponent},
-    {path : 'question', component : QuestionComponent},
-    {path : 'answer', component : AnswerComponent},
-
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent },
 ];
 
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+      RouterModule.forRoot(
+          routes,
+          {enableTracing: true}
+      )
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+}
