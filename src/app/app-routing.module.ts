6ee18b52d2608bd78000b453eac69a876c8eb233
sanggu <tkdrn8578@naver.com>
2019-11-30 17:50:44 +0900

    [MODIFY] 불필요한 router path 제거

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
-    // { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: HomeComponent },
 ];
 
