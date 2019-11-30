654598824df1990b4d15f4e2bd412c9bac25dfcb
sanggu <tkdrn8578@naver.com>
2019-11-30 17:54:01 +0900

    [MODIFY] platformBrowserDynamic 을 사용하지 못하는 이유로 라우팅이 되지 않아 기존 환경 설정으로 변경

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { hmrBootstrap } from 'hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-const bootstrap = () =>
-    platformBrowserDynamic()
-        .bootstrapModule(AppModule);
-
-if (environment.hmr) {
-  if (module['hot']) {
-      hmrBootstrap(module, bootstrap);
-  } else {
-    console.error('HMR 이 webpack-dev-server 을 실행할 수 없습니다.!');
-    console.log('ng server 에서 --hmr flag 를 사용한 것이 맞습니까?');
-  }
-}
-
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
