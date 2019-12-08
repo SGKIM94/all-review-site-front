1038a10e6e7d1c060b3d00dafd48895df0416f84
sanggu <tkdrn8578@naver.com>
2019-12-08 15:35:05 +0900

    [MODIFY] routes trace 설정 제거

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,8 +15,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
       RouterModule.forRoot(
-          routes,
-          {enableTracing: true}
+          routes
       )
   ],
   exports: [RouterModule]
