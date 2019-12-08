817ab39155356bae0c5a998944ea273ca7c9567f
sanggu <tkdrn8578@naver.com>
2019-12-08 17:55:59 +0900

    [MODIFY] 불필요한 import 제거

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,4 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-import { Platform } from '@angular/cdk/platform';
-import { TranslateService } from '@ngx-translate/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-
-import { FuseConfigService } from '@fuse/services/config.service';
-import { FuseNavigationService } from '@fuse/components/navigation/navigation.service';
-import { FuseSidebarService } from '@fuse/components/sidebar/sidebar.service';
-import { FuseSplashScreenService } from '@fuse/services/splash-screen.service';
-import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
-
-import { navigation } from 'app/navigation/navigation';
-import { locale as navigationEnglish } from 'app/navigation/i18n/en';
-import { locale as navigationTurkish } from 'app/navigation/i18n/tr';
 
 @Component({
   selector: 'app-root',
@@ -30,6 +15,4 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnInit(): void {}
 
   ngOnDestroy(): void {}
-
-  toggleSidebarOpen(key): void {}
 }
