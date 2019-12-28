b6399d769bd7d20b6e2bc1b3d658e4e953750e93
sanggu <tkdrn8578@naver.com>
2019-12-28 17:58:36 +0900

    [MODIFY] 회원가입을 위한 register routing 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', component: HomeComponent },
 ];
 
