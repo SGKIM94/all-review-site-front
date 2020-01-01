6d37de8301fd81023935de28b95a3de7815a6e83
sanggu <tkdrn8578@naver.com>
2020-01-01 23:28:34 +0900

    [MODIFY] register post 통신 메서드 추가

--- src/app/rest-config/login/login.service.ts
+++ src/app/rest-config/login/login.service.ts
@@ -3,7 +3,8 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, Observer, of} from 'rxjs';
 import {catchError, map, tap} from 'rxjs/operators';
 
-const endPoint = 'http://localhost:5000/api/users/login/';
+const endPoint = 'http://localhost:5000/api/users/';
+
 const httpOptions = {
     headers: new HttpHeaders({
         'Content-Type':  'application/json',
@@ -23,16 +24,19 @@ export class RestService {
         return response || {};
     }
 
-    login(user): Observable<any> {
+    register(user): Observable<any> {
         return this.http.post(endPoint, JSON.stringify(user), httpOptions)
+            .pipe (
+                catchError(this.handleError<any>('login'))
+            );
+    }
+
+
+    login(user): Observable<any> {
+        return this.http.post(endPoint + 'login/', JSON.stringify(user), httpOptions)
             .pipe (
             catchError(this.handleError<any>('login'))
         );
-    }
-
-    getUserData(): Observable<any> {
-        return this.http.get(endPoint + '/users/login', httpOptions).pipe (
-            map(this.extractData));
     }
 
     private handleError<T> (operation = 'operation', result?: T): (error: any) => Observable<T> {
