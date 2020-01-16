1ae83e66af970e2700c4b0ae103a0cd9c9593fa5
sanggu <tkdrn8578@naver.com>
2020-01-16 20:41:22 +0900

    [MODIFY] question 의 질문 게시판을 routing 에 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
+import {QuestionComponent} from './question/question.component';
 
 const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'question', component: QuestionComponent },
     { path: '**', component: HomeComponent },
 ];
 
