f37633d8c09df9fb8bc0561b1b837fc95a68a276
sanggu <tkdrn8578@naver.com>
2020-01-26 17:28:52 +0900

    [MODIFY] question list 에서 writer 를 가져오기 위해서 user class 추가

--- src/app/rest-config/user/user.service.ts
+++ src/app/rest-config/user/user.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, Observer, of} from 'rxjs';
 import {catchError, map, tap} from 'rxjs/operators';
+import {Questions} from '../question/question.service';
 
 const endPoint = 'http://localhost:5000/api/users/';
 
@@ -15,6 +16,20 @@ const httpOptions = {
 @Injectable({
     providedIn: 'root'
 })
+
+export class User {
+    userId: string;
+    password: string;
+    name: string;
+    email: string;
+
+    constructor(user) {
+        this.userId = user.userId;
+        this.password = user.password;
+        this.name = user.name;
+        this.email = user.email;
+    }
+}
 
 export class RestService {
     constructor(private http: HttpClient) {}
